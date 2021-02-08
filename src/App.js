61a20dff96dc20baf6030d77f712da4772e8b862
Kishkuman13 <amos.martineau@gmail.com>
2021-02-07 21:05:14 -0700

    fixed code

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import Character from './components/Character'
-import CharacterDetails from './components/CharacterDetails'
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -14,36 +13,10 @@ const App = () => {
 
 
   const [character, setCharacter] = useState([])
-  const [currentCharacter, setCurrentCharacter] = useState(0)
-
-  const viewCharacter = id => {
-    console.log(id.currentTarget)
-    var cache = []
-    const newChar = id.currentTarget
-    const newCharDet = JSON.stringify(newChar, (key, value) => {
-      if (typeof value === 'object' && value !== null) {
-        if (cache.includes(value)) return
-        cache.push(value)
-      }
-      return value
-    })
-    cache = null
-    setCurrentCharacter(newCharDet)
-    console.log((newCharDet))
-    // return newChar.replace(/\D/g,'')
-    // console.log(newChar)
-    // newChar = newChar.replace(/[^0-9]/g, '')
-    // console.log(newChar)
-  }
-
-  const closeCharacter = () => {
-    setCurrentCharacter(0)
-  }
 
   useEffect(() => {
     axios.get(`http://swapi.dev/api/people`)
       .then(res => {
-        console.log(res.data.results)
         setCharacter(res.data.results)
       })
       .catch(err => {
@@ -53,27 +26,18 @@ const App = () => {
 
   return (
     <div className="App">
-        <div className='container'>
-          <div className='left'>
-            <h1 className="Header">Characters</h1>
-      {
-        character.map((char, index) => {
-          return (
-              <Character key={index+10} id={index+10} info={char} action={viewCharacter} active={currentCharacter} close={closeCharacter} />
-          )
-        })
-      }
-      </div>
-      <div className='right'>
-      <h1 className="Header">Character Details</h1>
-      {
-        character.map((char, index) => {
-          return (
-      <CharacterDetails key={index} id={index} info={char} action={viewCharacter} active={currentCharacter} close={closeCharacter} />
-          )
-        })
-      }
-      </div>
+    <h1 className="Header">Characters</h1>
+      <div className='container'>
+        {
+          character.map((char, index) => {
+            return (
+              <Character 
+                key={index} 
+                id={char.name} 
+                info={char} />
+            )
+          })
+        }
       </div>
     </div>
   );
--- src/components/Character.js
+++ src/components/Character.js
@@ -3,22 +3,39 @@ import React from 'react'
 import styled from 'styled-components'
 
 const StyledCharacter = styled.div`
-    color:red;
-    text-align:left;
-    button {
-        text-align:right;
-        float:right;
+    font-family:monospace;
+    font-size:1.2em;
+    width: 25%;
+    margin: 1% auto;
+    background-color: rgba(240,240,240,.5);
+    padding:1% 2%;
+    border-radius: 5px;
+    color:rgb(190,55,60);
+
+    h3 {
+        color:rgb(190,55,60);
+    }
+
+    p {
+        margin:2px;
     }
 `
 
 const Character = (props) => {
-    const { info, action, id } = props
+    const { info } = props
     return (
-        <StyledCharacter>
-            <h3>Name: {info.name}
-            <button onClick={action} id={id}>View Details</button>
-            </h3>
-        </StyledCharacter>
+        <StyledCharacter >
+            <h3>{info.name}</h3>
+            <div>
+                <p>Birth Year: {info.birth_year}</p>
+                <p>Eye Color: {info.eye_color}</p>
+                <p>Hair Color: {info.hair_color}</p>
+                <p>Skin Color: {info.skin_color}</p>
+                <p>Gender: {info.gender}</p>
+                <p>Height: {info.height}</p>
+                <p>Mass: {info.mass}</p>
+            </div>
+        </StyledCharacter >
     )
 }
 
